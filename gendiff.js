01f25501f77d9256e1206dd997c4d3dc5abe5f26
ayankz <ayanpost87@gmail.com>
2023-06-22 01:28:53 +0600

    step#2

--- gendiff.js
+++ gendiff.js
@@ -5,6 +5,9 @@ const { program } = require('commander');
 program
     .helpOption('-h, --help', 'output usage information')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>' , 'output format')
     .option('-V, --version', 'output the version number')
+    .argument('<filepath1>' )
+    .argument('<filepath2>');
 
 program.parse();
