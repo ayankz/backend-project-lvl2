77271b6a95d13d45672a0173c4158ba8501dc116
ayankz <ayanpost87@gmail.com>
2023-06-28 02:20:05 +0600

    Step#4 add makefile, new action

--- gendiff.js
+++ gendiff.js
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
-import {program} from "commander";
+import { program } from 'commander';
 import path from 'path';
 import fs from 'fs';
-import genDiff from './main.js'
+import genDiff from './main';
+
 program
-    .helpOption('-h, --help', 'output usage information')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format <type>' , 'output format')
-    .option('-V, --version', 'output the version number')
-    .argument('<filepath1>' )
-    .argument('<filepath2>')
-    .action( (filepath1, filepath2)=>{
-        const data1 = fs.readFileSync(path.resolve(filepath1), 'utf-8');
-        const data2 = fs.readFileSync(path.resolve(filepath2), 'utf-8');
-        let result = genDiff(data1, data2);
-        console.log(`{\n ${result} }`)
-    })
+  .helpOption('-h, --help', 'output usage information')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .option('-V, --version', 'output the version number')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    const data1 = fs.readFileSync(path.resolve(filepath1), 'utf-8');
+    const data2 = fs.readFileSync(path.resolve(filepath2), 'utf-8');
+    const result = genDiff(data1, data2);
+    return `{\n ${result} }`;
+  });
 
 program.parse();
--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-import _ from 'lodash'
+import _ from 'lodash';
+
 export default (json1, json2) => {
-    console.log(json1);
-    console.log(json2);
-    const data1 = JSON.parse(json1);
+  const data1 = JSON.parse(json1);
 
-    const data2 = JSON.parse(json2);
+  const data2 = JSON.parse(json2);
 
-    const keys = _.union(Object.keys(data1), Object.keys(data2));
+  const keys = _.union(Object.keys(data1), Object.keys(data2));
 
-    return keys.reduce((differences, key) => {
-        if (_.isEqual(data1[key], data2[key])) {
-            differences += `   ${key}: ${data1[key] ?? data2[key]} \n`;
-        } else {
-            differences += _.has(data1, key) ? `  - ${key}: ${data1[key]} \n` : '';
-            differences += _.has(data2, key) ? `  + ${key}: ${data2[key]} \n` : '';
-        }
-        return differences;
-    }, '');
-}
+  return keys.reduce((differences, key) => {
+    if (_.isEqual(data1[key], data2[key])) {
+      differences += `   ${key}: ${data1[key] ?? data2[key]} \n`;
+    } else {
+      differences += _.has(data1, key) ? `  - ${key}: ${data1[key]} \n` : '';
+      differences += _.has(data2, key) ? `  + ${key}: ${data2[key]} \n` : '';
+    }
+    return differences;
+  }, '');
+};
